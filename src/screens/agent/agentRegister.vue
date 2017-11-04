a3d4b967927bcaeb088149f75a297b22aaba97df
kayne@unnotech.com <kayne@unnotech.com>
2017-11-04 17:05:09 +0800

    add tooltips for username and password field & fix captcha always false > /agent/agentRegister.vue

--- src/screens/agent/agentRegister.vue
+++ src/screens/agent/agentRegister.vue
@@ -1,44 +1,53 @@
 <template>
-  <div class="agent-register">
-    <el-steps :active="0" finish-status="success"  simple>
-      <el-step title="代理註冊" icon="el-icon-edit" ></el-step>
-      <el-step title="註冊完成" icon="el-icon-success"></el-step>
-      <el-step title="代理登入" icon="el-icon-star-on" ></el-step>
-    </el-steps>
-      <el-row type="flex" class="row-bg" justify="center">
-          <el-col :span="8">
-            <el-form :label-position="labelPosition" :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-              <el-form-item label="代理帳號設定" prop="username">
-                <el-input v-model.number="ruleForm.username"></el-input>
-              </el-form-item>
+  <div>
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-col :span="8">
+        <el-form :label-position="labelPosition" 
+                 :model="ruleForm" 
+                 status-icon
+                 :rules="formRules"
+                 ref="ruleForm"
+                 label-width="100px">
+          <el-tooltip content="6~10个字符的代理帐号" placement="right">
+            <el-form-item label="代理帐号" prop="username">
+              <el-input v-model.number="ruleForm.username"></el-input>
+            </el-form-item>
+          </el-tooltip>
+            <el-tooltip content="密码请至少含有两个字母且4-13个字符" placement="right">
               <el-form-item label="設定密码" prop="password">
                 <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
               </el-form-item>
-              <el-form-item label="确认密码" prop="confirm_password">
-                <el-input type="password" v-model="ruleForm.confirm_password" auto-complete="off"></el-input>
-              </el-form-item>
-              <el-tooltip class="item" content="真实姓名必须与您用于提款银行账户名称一致唷" placement="right">
-              <el-form-item label="真實姓名" prop="real_name">
+            </el-tooltip>
+            <el-form-item label="确认密码" prop="confirm_password">
+              <el-input type="password" v-model="ruleForm.confirm_password" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-tooltip content="提醒：真实姓名必须与您用于提款银行账户名称一致唷" placement="right">
+              <el-form-item label="真实姓名" prop="real_name">
                 <el-input v-model="ruleForm.real_name"> </el-input>
               </el-form-item>
-              </el-tooltip>
-              <el-form-item label="手機號碼" prop="phone">
-                <el-input v-model="ruleForm.phone" number></el-input>
+            </el-tooltip>
+            <el-form-item label="手机号码" prop="phone">
+              <el-input v-model="ruleForm.phone" number></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+              <el-input v-model="ruleForm.email"></el-input>
+            </el-form-item>
+            <el-tooltip placement="right" effect="light" :value="captcha.show" :manual="true">
+              <div slot="content">
+                <img :src="captcha.img" @click="grabCaptcha()">
+                <br/>点击图片换一张</div>
+              <el-form-item label="验证码" prop="captcha_1">
+                <el-input v-model="ruleForm.captcha_1" 
+                          @focus="captcha.show = true" 
+                          @blur="captcha.show = false" 
+                          @keyup.enter.native="submitForm('ruleForm')"></el-input>
               </el-form-item>
-              <el-form-item label="郵箱" prop="email">
-                <el-input v-model="ruleForm.email"></el-input>
-              </el-form-item>
-              <el-tooltip class="item" placement="right" effect="light" :value="captcha.show">
-              <div slot="content" manual="true"><img :src="captcha.img" @click="reCaptcha()"></div>
-              <el-form-item label="驗證碼">
-                <el-input v-model="ruleForm.captcha_1"  @focus="captcha.show = true" @blur="captcha.show = false"></el-input> 
-              </el-form-item>  
-              </el-tooltip>
-              <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')" size= "mini">提交</el-button>
-              </el-form-item>
-            </el-form>
-          </el-col>    
+            </el-tooltip>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')" size="medium">提交</el-button>
+            </el-form-item>
+        </el-form>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -50,39 +59,7 @@
   
   export default {
     data () {
-      var checkusername = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('不能为空'))
-        }
-        setTimeout(() => {
-          if (value.length > 16 || value.length < 7) {
-            callback(new Error('帐户名由6-15个字符组成'))
-          }
-          callback()
-        }, 1000)
-      }
-      // var checkname = (rule, value, callback) => {
-      //   if (!value) {
-      //     return callback(new Error('不能为空'))
-      //   }
-      //   setTimeout(() => {
-      //     if () {
-      //       callback(new Error('帐户名由6-15个字符组成'))
-      //     }
-      //     callback()
-      //   }, 1000)
-      // }
       var validatePass = (rule, value, callback) => {
-        if (!value) {
-          callback(new Error('请输入密码'))
-        } else {
-          if (this.ruleForm.confirm_password !== '') {
-            this.$refs.ruleForm.validateField('confirm_password')
-          }
-          callback()
-        }
-      }
-      var validatePass2 = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请再次输入密码'))
         } else if (value !== this.ruleForm.password) {
@@ -106,39 +83,50 @@
           img: '',
           show: false
         },
-        rules: {
+        formRules: {
+          username: [{
+            required: true,
+            pattern: /^[a-zA-Z0-9]{4,10}$/,
+            message: '6~10个字符的代理帐号',
+            trigger: 'blur'
+          }],
           password: [{
-            validator: validatePass,
+            required: true,
+            pattern: /^[a-zA-Z]{2}[a-zA-Z0-9]{4,13}$/,
+            message: '密码请至少含有两个字母且4-13个字符',
             trigger: 'blur'
           }],
           confirm_password: [{
-            validator: validatePass2,
+            required: true,
+            validator: validatePass,
             trigger: 'blur'
           }],
-          username: [{
-            validator: checkusername,
+          real_name: [{
+            required: true,
+            pattern: /^\D*$/,
+            message: '请输入真实姓名',
             trigger: 'blur'
           }],
           phone: [{
+            required: true,
             pattern: /^\d{11}$/,
-            message: '请输入 11 位手机号'
+            message: '请输入 11 位手机号',
+            trigger: 'blur'
+          }],
+          email: [{
+            required: true,
+            pattern: /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/,
+            message: '请输入正确格式的邮箱',
+            trigger: 'blur'
+          }],
+          captcha_1: [{
+            required: true,
+            message: '请输入验证码',
+            trigger: 'blur'
           }]
         },
-        labelPosition: 'left',
-        isLoading: false
+        labelPosition: 'left'
       }
-    },
-    created () {
-      getCaptcha().then(
-        response => {
-          this.captcha.show = true
-          this.ruleForm.captcha_0 = response.data.captcha_val
-          this.captcha.img = api.domain + response.data.captcha_src
-        },
-        response => {
-          console.log('capture lost')
-        }
-      )
     },
     methods: {
       submitForm (formName) {
@@ -147,32 +135,56 @@
             let inputData = this.ruleForm
             register(inputData).then(
               response => {
-                console.log('post success')
-                console.log(response)
-              }, response => {
-              console.log(response)
-              console.log('post fail')
-              console.log(this.$route.path.push)
-              this.$route.path.push('/success')
+                this.$router.push({name: 'Home'})
+              }, errorRes => {
+              const errors = errorRes.response.data.error
+              let messages = []
+
+              errors.forEach(error => {
+                Object.keys(error).forEach(key => {
+                  messages.push(error[key])
+                })
+              })
+
+              this.$message({
+                showClose: true,
+                message: messages.join(', '),
+                type: 'error'
+              })
             })
           } else {
-            console.log('error submit!!')
             return false
           }
         })
       },
-      reCaptcha () {
-        this.captcha.show = true
+      grabCaptcha () {
         getCaptcha().then(
         response => {
+          this.captcha.show = true
           this.ruleForm.captcha_0 = response.data.captcha_val
           this.captcha.img = api.domain + response.data.captcha_src
         },
-        response => {
-          console.log('capture lost')
+        errorRes => {
+          const errors = errorRes.response.data.error
+          let messages = []
+
+          errors.forEach(error => {
+            Object.keys(error).forEach(key => {
+              messages.push(error[key])
+            })
+          })
+
+          this.$message({
+            showClose: true,
+            message: messages.join(', '),
+            type: 'error'
+          })
         }
       )
       }
+    },
+    created () {
+      this.grabCaptcha()
     },
     components: {
       TopBar
@@ -181,10 +193,6 @@
 </script>
 
 <style lang="sass" scoped>
-.el-row
-  border-radius: 3px
-  box-shadow: 0px 0px 3px rgba(0,0,0,.1)
-
 .row-bg
   padding: 10px 0
   background-image: linear-gradient(180deg, #fdfbfb 0%, #ebedee 100%)
