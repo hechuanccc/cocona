f8075a4243170e7f005232eefad25e9af0c86f51
kayne@unnotech.com <kayne@unnotech.com>
2017-11-06 15:25:00 +0800

    import i18n > /i18n/

---
+++ src/i18n/cn.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export default {
+  agent: {
+    username: '代理帐号',
+    password: '設定密码',
+    confirm_password: '确认密码',
+    real_name: '真实姓名',
+    realname_tip: '提醒：真实姓名必须与您用于提款银行账户名称一致',
+    phone: '手机号码',
+    email: '邮箱',
+    captcha: '验证码',
+    captcha_reset: '点击图片换一张'
+  }
+}
---
+++ src/i18n/en.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export default {
+  agent: {
+    username: 'Username',
+    password: 'Password',
+    confirm_password: 'Confirm',
+    real_name: 'Real Name',
+    realname_tip: 'Tips: Must be the same as the name you used for your withdrawal bank account',
+    phone: 'Phone',
+    email: 'Email',
+    captcha: 'Captcha',
+    captcha_reset: 'Reset Captcha'
+  }
+}
