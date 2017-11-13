eda33b774d6ca8dcc0a7e143f89e48d2a943a692
kayne@unnotech.com <kayne@unnotech.com>
2017-11-13 13:58:20 +0800

    rename capital

---
+++ src/components/HeadBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="top-header">
+    <div class="header-bar">
+        <TopBar v-show="isFrontPage"/>
+    </div>
+    <div class="header-nav container">
+        <Logo/>
+        <NavMenu v-if="isFrontPage" />
+        <NavMenuBack v-else/>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.top-header
+  margin-bottom: 10px
+  .header-bar
+    width: 100%
+    background: #433e81
+    text-align: center
+  .header-nav
+    margin-top: 10px
+</style>
+<script>
+import TopBar from '../components/TopBar'
+import NavMenu from '../components/NavMenu'
+import Logo from '../components/Logo'
+import NavMenuBack from '../components/NavMenuBack'
+
+export default {
+  data () {
+    return {
+      userInfo: ''
+    }
+  },
+  components: {
+    TopBar,
+    NavMenu,
+    Logo,
+    NavMenuBack
+  },
+  computed: {
+    isFrontPage () {
+      return this.$route.path.indexOf('/game/') === -1 && this.$route.path.indexOf('/account/') === -1
+    }
+  }
+}
+</script>
+
+
