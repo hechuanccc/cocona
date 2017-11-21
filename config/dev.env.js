5b5e8f05ee8cec14db6ece5050b281decc349742
Dean Christian <dean@unnotech.com>
2017-11-21 15:47:51 +0800

    Change the backend to localhost in dev environment

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  HOST: '"http://2689df54-0e1b-4a12-91ef-b853a47bab34.node.dockerapp.io:8000"'
+  HOST: '"http://localhost:8000"'
 })
