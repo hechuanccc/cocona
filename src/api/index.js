2bf10e6336ae09ef74ddd850d59709c046cb94a8
jerrywei <jerrywei17@unnotech.com>
2017-12-21 10:00:38 +0800

    fix gamehistory split page

--- src/api/index.js
+++ src/api/index.js
@@ -162,8 +162,14 @@ export function readMessage (ids) {
     { 'Content-Type': 'application/json' })
 }
 
-export function fetchHistory (gameCode, date, limit, offset) {
-  return axios.get(`${urls.gamehistory}?game_code=${gameCode}&date=${date}&limit=${limit}&offset=${offset}`)
+export function fetchHistory (option) {
+  let url = `${urls.gamehistory}?game_code=${option.game_code}&limit=30`
+  Object.keys(option).forEach(key => {
+    if (option[key]) {
+      url += `&${key}=${option[key]}`
+    }
+  })
+  return axios.get(url)
 }
 
 export function fetchPlaySetting (id) {
