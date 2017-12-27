d9f4dea26986c41fb9086579af2ee88802413ed7
kayne@unnotech.com <kayne@unnotech.com>
2017-12-27 15:16:30 +0800

    minor

--- src/screens/games/Game.vue
+++ src/screens/games/Game.vue
@@ -10,7 +10,7 @@
           </div>
           <div class="schedule" v-if="schedule && schedule.issue_number">
             <div class="schedule-title">封盘</div>
-            <span v-if="!gameClosed" class="red countdown">
+            <span v-if="!gameClosed" class="red countdown" :style="{ fontSize: closeCountDown.days > 0 ? '15px': '18px' }">
               <span v-if="closeCountDown.days > 0">{{closeCountDown.days}}天 </span>
               <span v-if="closeCountDown.hours > 0">{{closeCountDown.hours | complete}}:</span>{{closeCountDown.minutes | complete}}:{{closeCountDown.seconds | complete}}
             </span>
@@ -18,7 +18,7 @@
           </div>
           <div class="schedule" v-if="schedule && schedule.issue_number">
             <div class="schedule-title">开奖</div>
-            <span v-if="!ended" class="green countdown">
+            <span v-if="!ended" class="green countdown" :style="{ fontSize: closeCountDown.days > 0 ? '15px': '18px' }">
               <span v-if="resultCountDown.days > 0">{{resultCountDown.days}}天 </span>
               <span v-if="resultCountDown.hours > 0">{{resultCountDown.hours | complete}}:</span>{{resultCountDown.minutes | complete}}:{{resultCountDown.seconds | complete}}
             </span>
