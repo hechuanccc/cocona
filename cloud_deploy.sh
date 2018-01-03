eef7cce69f95e7f955e058351e115786417411f3
Dean Christian <dean@unnotech.com>
2018-01-03 18:31:28 +0800

    enable build failure if script is unsuccessful

--- cloud_deploy.sh
+++ cloud_deploy.sh
@@ -8,12 +8,12 @@ export static_container=static
 
 # Needed to avoid piling up of data in image and Azure file storage
 rm -rf dist
-/root/bin/az storage blob delete-batch --source $static_container
 
 npm install
 npm run build
 
 # Take note that the format URL for the storage account is {storage-url}/{blob-container} and $root is the only way to avoid the "blob container" format and the other static holders need to be declared as blobs to maintain the folder structure
+/root/bin/az storage blob delete-batch --source $static_container
 /root/bin/az storage container create --public-access blob --name $root_container
 /root/bin/az storage container create --public-access blob --name $static_container
 
