702f7315f79a01e86a59be1f2a3f222f73be28aa
Dean Christian <dean@unnotech.com>
2018-01-26 14:31:04 +0800

    initial setup

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ COPY . .
 
 ARG AZURE_STORAGE_ACCOUNT
 ARG AZURE_STORAGE_ACCESS_KEY
+ARG ENV_CONTAINER
 ARG CDN_PROFILE
 ARG CDN_ENDPOINT
 ARG HOST
@@ -24,7 +25,7 @@ ARG MAX_AGE
 
 # Cannot be made into one line as it has a possibility that it will return a 'text file busy' making the shell script unexecutable
 RUN chmod u+x cloud_deploy.sh
-RUN ./cloud_deploy.sh
+# RUN ./cloud_deploy.sh
 
 EXPOSE 8080
 CMD npm run dev
--- cloud_deploy.sh
+++ cloud_deploy.sh
@@ -3,6 +3,7 @@ set -e  # to stop script when error occurs
 
 # NPM commands and Azure cli scripts to deploy our static VueJS in a CDN
 
+# export ENV_CONTAINER=staging
 export root_container=\$root  # is '\' is to escape the special character
 export static_container=static
 
@@ -13,14 +14,12 @@ npm install
 npm run build
 
 # Take note that the format URL for the storage account is {storage-url}/{blob-container} and $root is the only way to avoid the "blob container" format and the other static holders need to be declared as blobs to maintain the folder structure
-/root/bin/az storage container create --public-access blob --name $root_container
-/root/bin/az storage container create --public-access blob --name $static_container
-
+/root/bin/az storage container create --public-access blob --name $ENV_CONTAINER
 
 # Upload the changes
-/root/bin/az storage blob delete-batch --source $static_container
-/root/bin/az storage blob upload-batch --content-cache-control "public, max-age=$MAX_AGE" --destination $static_container --source dist/static
-/root/bin/az storage blob upload --file dist/index.html --container-name $root_container  --name index.html
+/root/bin/az storage blob delete-batch --source $ENV_CONTAINER --pattern '$static_container/*'
+/root/bin/az storage blob upload-batch --content-cache-control "public, max-age=$MAX_AGE" --destination $ENV_CONTAINER/$static_container --source dist/static
+/root/bin/az storage blob upload --file dist/index.html --container-name $ENV_CONTAINER/$root_container  --name index.html
 
 
 # To start purging the CDN
