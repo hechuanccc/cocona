fd02626d5d2011f51bfdcaf4e0d14d485994ea26
kayne@unnotech.com <kayne@unnotech.com>
2018-01-26 09:53:26 +0800

    add login verification

--- src/main.js
+++ src/main.js
@@ -73,8 +73,10 @@ axios.interceptors.response.use(res => {
   } else {
     if (responseData.code === 9007) {
       toHomeAndLogin(router)
-    } else if (responseData.code === 9011) { // for login fail: "Invalid username or password"
+    } else if (responseData.code === 9011 || responseData.code === 9013) { // for login fail: "Invalid username or password"
+      axios.defaults.withCredentials = true
       Vue.cookie.set('sessionid', res.data.data.sessionid)
+      return Promise.reject(responseData)
     }
     return Promise.reject(responseData.msg)
   }
