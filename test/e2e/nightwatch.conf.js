d1d28366fc5a24c92f343bf1362ea8fa27d6b8d0
jerrywei <jerrywei17@unnotech.com>
2018-05-31 09:37:15 +0800

    add login test

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -2,6 +2,7 @@ require('babel-register')
 var config = require('../../config')
 
 // http://nightwatchjs.org/gettingstarted#settings-file
+
 module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
